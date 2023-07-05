086212fcf24074a62697910ac2aacf14e4a8856d
SorinGFS <sorin.gfs@protonmail.com>
2023-07-05 22:04:23 +0300

    fix add schema if initial build does not exist

--- actions/add/schema/index.js
+++ actions/add/schema/index.js
@@ -4,6 +4,10 @@ const fs = require('zerodep/node/fs');
 const fn = require('zerodep/node/fn');
 // arg must be directory pathResolve arg
 const schema = require(`${process.env.PWD}/base/schema`)(process.argv[2]);
-const built = require(`${process.env.PWD}/index.json`);
-fs.writeFile('index.json', JSON.stringify(fn.structuredClone(Object.assign({}, built, schema)), false, 4));
+try {
+    const built = require(`${process.env.PWD}/index.json`);
+    fs.writeFile('index.json', JSON.stringify(fn.structuredClone(Object.assign({}, built, schema)), false, 4));
+} catch (error) {
+    fs.writeFile('index.json', JSON.stringify(fn.structuredClone(schema), false, 4));
+}
 console.log(`Schema from '${process.argv[2]}' successfully compiled.`);
